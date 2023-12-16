17d22e1c88fa8a11c3e6c6dcce0e3c82dafb397f
Saath Satheeshkumar <saathsatheesh@gmail.com>
2023-12-16 14:39:11 +0000

    feat: Add background audio to clips

--- src/main.py
+++ src/main.py
@@ -1,14 +1,37 @@
 import whisper_timestamped
+from moviepy.audio.fx.volumex import volumex
 from moviepy.audio.io.AudioFileClip import AudioFileClip
-from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
+from moviepy.video.compositing.CompositeVideoClip import (
+    CompositeAudioClip,
+    CompositeVideoClip,
+)
 from moviepy.video.fx.crop import crop
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.video.VideoClip import TextClip
 
+# value is (value to scale audio volume, seconds into audio to use)
+scaleAudioClipProperties = {"resources/tiktokGymPhonk.mp3": (0.1, 25)}
 
-def addSubtitles(subs, video_clip_path):
-    video_clip = VideoFileClip(video_clip_path, target_resolution=(600, 800))
+
+# Return an audio clip with the audio from path + the original audio
+def createAudio(audio_clip_path, video_clip):
+    if audio_clip_path not in scaleAudioClipProperties:
+        raise Exception("Audio clip not found in scaleAudioClipProperties")
+    (volume_scale, secondsIntoClip) = scaleAudioClipProperties[audio_clip_path]
+    original_audio = video_clip.audio
+    audio_clip = AudioFileClip(audio_clip_path).subclip(
+        secondsIntoClip, secondsIntoClip + video_clip.duration
+    )
+    audio_clip = volumex(audio_clip, volume_scale)
+    audio = CompositeAudioClip([audio_clip, original_audio])
+    return audio
+
+
+def createVideo(subs, video_clip_path, audio_clip_path):
+    video_clip = VideoFileClip(video_clip_path, target_resolution=(900, 800))
+    audio = createAudio(audio_clip_path, video_clip)
+    video_clip = video_clip.set_audio(audio)
     result = CompositeVideoClip([video_clip, subs.set_pos(("center", "center"))])
     result.write_videofile(
         "content/output.mp4",
@@ -43,8 +66,7 @@ def run_whisper(file_path: str):
             subs.append(((start_time, end_time), text))
 
     subtitles = subtitle_generator(subs)
-    addSubtitles(subtitles, file_path)
+    createVideo(subtitles, file_path, "resources/tiktokGymPhonk.mp3")
 
 
-run_whisper("content/sam_sulek.mp4")
-# extract_audio_from_video("content/sam_sulek.mp4", "content/sam_sulek.mp3")
+run_whisper("")
